e6cfbbc68aae9eee4ddfbde83117ae78fb9f0965
Dannybl8ze <sdfridaysd@gmail.com>
2023-10-02 22:35:51 +0000

    stat Lib

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - the Entry point
+ *
+ * @s: an input
+ * @c: an input
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int j = 0;
+
+	for (; s[j] >= '\0'; j++)
+	{
+		if (s[j] == c)
+			return (&s[j]);
+	}
+	return (0);
+}
